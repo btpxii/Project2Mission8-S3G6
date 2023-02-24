a00294283c53d0d299d374676deb8fc1bcd5faae
btpxii <btpaul12@gmail.com>
2023-02-23 21:05:25 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project2Mission8_S3G6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
