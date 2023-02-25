b21b6346eab002e6639e3a95736263fd3629f97d
Eliza Greer <eg439@byu.edu>
2023-02-24 18:13:26 -0700

    gay

---
+++ Migrations/20230224231212_Initial.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project2Mission8_S3G6.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TaskName = table.Column<string>(nullable: false),
+                    DueDate = table.Column<string>(nullable: true),
+                    Quadrant = table.Column<int>(nullable: false),
+                    Completed = table.Column<bool>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.TaskId);
+                    table.ForeignKey(
+                        name: "FK_Responses_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 1, "Home" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 2, "School" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 3, "Work" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 4, "Church" });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "TaskId", "CategoryId", "Completed", "DueDate", "Quadrant", "TaskName" },
+                values: new object[] { 1, 1, false, null, 3, "do dishes" });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "TaskId", "CategoryId", "Completed", "DueDate", "Quadrant", "TaskName" },
+                values: new object[] { 2, 1, true, null, 1, "clean bathroom" });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "TaskId", "CategoryId", "Completed", "DueDate", "Quadrant", "TaskName" },
+                values: new object[] { 3, 4, false, null, 2, "ministring" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Responses_CategoryId",
+                table: "Responses",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ Models/TaskContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project2Mission8_S3G6.Models
+{
+    public class TaskContext :DbContext
+    {
+        //constructor
+        public TaskContext(DbContextOptions<TaskContext> options) : base (options)
+        {
+           
+        }
+
+        public DbSet<Task> Responses { get; set;}
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Home" },
+                new Category { CategoryId = 2, CategoryName = "School" },
+                new Category { CategoryId = 3, CategoryName = "Work" },
+                new Category { CategoryId = 4, CategoryName = "Church" }
+                );
+
+
+            mb.Entity<Task>().HasData(
+                new Task
+                {
+                    TaskId = 1,
+                    TaskName = "do dishes",
+                    Quadrant = 3,
+                    Completed = false,
+                    CategoryId =1
+                },
+                new Task
+                {
+                    TaskId = 2,
+                    TaskName = "clean bathroom",
+                    Quadrant = 1,
+                    Completed = true,
+                    CategoryId = 1
+                },
+                new Task
+                {
+                    TaskId = 3,
+                    TaskName = "ministring",
+                    Quadrant = 2,
+                    Completed = false,
+                    CategoryId = 4
+                }
+                );
+        }
+    }
+}
